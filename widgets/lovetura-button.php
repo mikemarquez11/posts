2ca806bb231d36848ba23aee5c1784f8ca06f921
Mike Marquez <mike.marquez11@gmail.com>
2020-05-28 21:02:36 -0500

    Adding Widget for Mailjet Popin Form

---
+++ widgets/lovetura-button.php
@@ -0,0 +1,360 @@
+<?php
+namespace Elementor_LovePosts\Widgets;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Core\Schemes;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Text_Shadow;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Box_Shadow;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * @since 1.2.0
+ */
+class LoveButton extends Widget_Base {
+
+    /**
+     * Retrieve the widget name.
+     * 
+     * @since 1.2.0
+     * @access public
+     * @return string Widget name.
+     */
+    public function get_name() {
+        return 'LoveButton';
+    }
+
+    /**
+     * Retrieve the widget title.
+     * 
+     * @since 1.2.0
+     * @access public
+     * @return string Widget title.
+     */
+    public function get_title() {
+        return __( 'Lovetura Button', 'elementor-love-button' );
+    }
+
+    /**
+     * Retrieve the widget icon.
+     * 
+     * @since 1.1.0
+     * @access public
+     * @return string Widget icon.
+     */
+    public function get_icon() {
+        return 'eicon-button';
+    }
+
+    /**
+     * Retrieve the list of categories the widget belongs to.
+     *
+     * Used to determine where to display the widget in the editor.
+     *
+     * Note that currently Elementor supports only one category.
+     * When multiple categories passed, Elementor uses the first one.
+     *
+     * @since 1.1.0
+     *
+     * @access public
+     *
+     * @return array Widget categories.
+     */
+    public function get_categories() {
+        return [ 'lovetura' ];
+    }
+
+    /**
+	 * Get button sizes.
+	 *
+	 * Retrieve an array of button sizes for the button widget.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @static
+	 *
+	 * @return array An array containing button sizes.
+	 */
+	public static function get_button_sizes() {
+		return [
+			'xs' => __( 'Extra Small', 'elementor' ),
+			'sm' => __( 'Small', 'elementor' ),
+			'md' => __( 'Medium', 'elementor' ),
+			'lg' => __( 'Large', 'elementor' ),
+			'xl' => __( 'Extra Large', 'elementor' ),
+		];
+    }
+
+    public function get_script_depends() {
+        $scripts = array( 'lt-mj' );
+     
+        return $scripts;
+    }
+
+    /**
+     * Register the widget controls.
+     * 
+     * Adds different input fields to allow the user to change and customize the widget settings.
+     * 
+     * @since 1.1.0
+     * @access protected
+     */
+    protected function _register_controls() {
+
+        $this->start_controls_section(
+            'section_content',
+            [
+              'label' => __( 'Button', 'elementor-love-button' ),
+            ]
+          );
+       
+          $this->add_control(
+            'title',
+            [
+              'label' => __( 'Text', 'elementor-love-button' ),
+              'type' => Controls_Manager::TEXT,
+              'default' => __( 'Guía gratuita', 'elementor-love-button' ),
+            ]
+          );
+
+          $this->add_control(
+			'size',
+			[
+				'label' => __( 'Size', 'elementor' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'sm',
+				'options' => self::get_button_sizes(),
+				'style_transfer' => true,
+			]
+		  );
+       
+          $this->end_controls_section();
+
+
+          $this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Button', 'elementor' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+          );
+
+          $this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'scheme' => Schemes\Typography::TYPOGRAPHY_4,
+				'selector' => '{{WRAPPER}} .elementor-button',
+			]
+          );
+
+          $this->add_group_control(
+			Group_Control_Text_Shadow::get_type(),
+			[
+				'name' => 'text_shadow',
+				'selector' => '{{WRAPPER}} .elementor-button',
+			]
+          );
+
+        $this->start_controls_tabs( 'tabs_button_style' );
+
+		$this->start_controls_tab(
+			'tab_button_normal',
+			[
+				'label' => __( 'Normal', 'elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'button_text_color',
+			[
+				'label' => __( 'Text Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'default' => '',
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button' => 'fill: {{VALUE}}; color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'background_color',
+			[
+				'label' => __( 'Background Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => Schemes\Color::get_type(),
+					'value' => Schemes\Color::COLOR_4,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button' => 'background-color: {{VALUE}};',
+				],
+			]
+		);
+
+        $this->end_controls_tab();
+
+        $this->start_controls_tab(
+			'tab_button_hover',
+			[
+				'label' => __( 'Hover', 'elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'hover_color',
+			[
+				'label' => __( 'Text Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button:hover, {{WRAPPER}} .elementor-button:focus' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .elementor-button:hover svg, {{WRAPPER}} .elementor-button:focus svg' => 'fill: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'button_background_hover_color',
+			[
+				'label' => __( 'Background Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button:hover, {{WRAPPER}} .elementor-button:focus' => 'background-color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'button_hover_border_color',
+			[
+				'label' => __( 'Border Color', 'elementor' ),
+				'type' => Controls_Manager::COLOR,
+				'condition' => [
+					'border_border!' => '',
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button:hover, {{WRAPPER}} .elementor-button:focus' => 'border-color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'hover_animation',
+			[
+				'label' => __( 'Hover Animation', 'elementor' ),
+				'type' => Controls_Manager::HOVER_ANIMATION,
+			]
+		);
+
+		$this->end_controls_tab();
+
+        $this->end_controls_tabs();
+
+        $this->add_group_control(
+			Group_Control_Border::get_type(),
+			[
+				'name' => 'border',
+				'selector' => '{{WRAPPER}} .elementor-button',
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_control(
+			'border_radius',
+			[
+				'label' => __( 'Border Radius', 'elementor' ),
+				'type' => Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Box_Shadow::get_type(),
+			[
+				'name' => 'button_box_shadow',
+				'selector' => '{{WRAPPER}} .elementor-button',
+			]
+		);
+
+		$this->add_responsive_control(
+			'text_padding',
+			[
+				'label' => __( 'Padding', 'elementor' ),
+				'type' => Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-button' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+				'separator' => 'before',
+			]
+		);
+
+        
+        $this->end_controls_section();
+
+    }
+
+    /**
+   * Render the widget output on the frontend.
+   *
+   * Written in PHP and used to generate the final HTML.
+   *
+   * @since 1.1.0
+   *
+   * @access protected
+   */
+  protected function render() {
+
+    $settings = $this->get_settings_for_display();
+
+    $this->add_render_attribute( 'wrapper', 'class', 'elementor-button-wrapper' );
+
+    $this->add_render_attribute( 'button', 'class', 'elementor-button' );
+    $this->add_render_attribute( 'button', 'role', 'button' );
+
+    if ( ! empty( $settings['size'] ) ) {
+        $this->add_render_attribute( 'button', 'class', 'elementor-size-' . $settings['size'] );
+    }
+ 
+    $this->add_inline_editing_attributes( 'title', 'none' ); ?>
+
+    <data id="mj-w-res-data" data-token="582b1969670786d7e11c02933031f93f" class="mj-w-data" data-apikey="5cnQ" data-w-id="DB6" data-lang="es_ES" data-base="https://app.mailjet.com" data-width="640" data-height="663.469" data-statics="statics"></data>
+
+    <div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
+    <a <?php echo $this->get_render_attribute_string( 'button' ); ?> href="#" data-token="582b1969670786d7e11c02933031f93f" onclick="mjOpenPopin(event, this)"><?php echo $settings['title']; ?></a>
+    </div>
+ 
+    <?php
+  }
+
+  /**
+   * Render the widget output in the editor.
+   *
+   * Written as a Backbone JavaScript template and used to generate the live preview.
+   *
+   * @since 1.1.0
+   *
+   * @access protected
+   */
+  protected function _content_template() {
+    ?>
+    <#
+    view.addInlineEditingAttributes( 'title', 'none' );
+    #>
+    
+    <div class="elementor-button-wrapper">
+        <a class="elementor-button elementor-size-{{ settings.size }}" {{{ view.getRenderAttributeString( 'title', 'button' ) }}} href="#" data-token="582b1969670786d7e11c02933031f93f" onclick="mjOpenPopin(event, this)">{{{ settings.title }}}</a>
+    </div>
+    <?php
+  }
+}
